b25243484746bd5c0f2fd1593b2758637b1a0522
Nikita <ryabokon.niki08@yandex.com>
2026-02-18 11:54:19 +0300

    base done

--- base.py
+++ base.py
@@ -1,19 +1,31 @@
-
-
+CONSTS = {"g": 9.81, "p": 1029.3, "b": 1.5, "n": 0.4}
 # Константы
-g = 9.81
-p = 1029.3
-b = 1.5
-n = 0.4
+# g = 9.81
+# p = 1029.3
+# b = 1.5
+# n = 0.4
 # Ввод
 H = 20
 Vt = 0.00135
 t = 20
 
 
-Np = p*g*Vt*t
-print(Np)
-Ndv =  round((Np/1000),2)/n
-print(Ndv)
-Nust = b *Ndv
-print(round(Nust,1))
+def useful_power(Vt, temperature):
+    return round(CONSTS["p"] * CONSTS["g"] * temperature * Vt / 1000, 2)
+
+
+def nominal_power(u_p):
+    return u_p / CONSTS["n"]
+
+
+def installed_capacity(nom_power):
+    return round(CONSTS["b"] * nom_power), round(CONSTS["b"] * nom_power, 2)
+
+up = useful_power(Vt, t)
+print(up)
+
+np = nominal_power(up)
+print(np)
+
+ic = installed_capacity(np)
+print(ic)
