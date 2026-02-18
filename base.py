f56e48339da79f519534dd6ae3b5296015441783
Ezik-ov <mmm807492@gmail.com>
2026-02-18 11:39:40 +0300

    sdfdsgggg

--- base.py
+++ base.py
@@ -3,7 +3,8 @@
 # Константы
 g = 9.81
 p = 1029.3
-
+b = 1.5
+n = 0.4
 # Ввод
 H = 20
 Vt = 0.00135
@@ -12,3 +13,7 @@ t = 20
 
 Np = p*g*Vt*t
 print(Np)
+Ndv =  round((Np/1000),2)/n
+print(Ndv)
+Nust = b *Ndv
+print(round(Nust,1))
