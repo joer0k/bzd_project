7ae9b4f37d031d48ecbb6ac31f45167109d8771d
Ezik-ov <mmm807492@gmail.com>
2026-02-26 19:36:07 +0300

    Шрифт + ctk

--- ctk.py
+++ ctk.py
@@ -12,7 +12,7 @@ class App(ctk.CTk):
         self.title("Базовая программа.py")
         self.geometry("400x200")
         self.entry = ctk.CTkEntry(self, placeholder_text = "text")
-        self.entry.pack(pady(20)
+        self.entry.pack(pady=20)
         # Добавление виджетов
         self.label = ctk.CTkLabel(self, text="Привет, CustomTkinter!", font=("Arial", 20))
         self.label.pack(pady=20)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
-
+from base import *
+from tkinter import messagebox
 # Настройка внешнего вида (System, Dark, Light)
 ctk.set_appearance_mode("Light")
 ctk.set_default_color_theme("green")
@@ -8,26 +9,39 @@ class App(ctk.CTk):
     def __init__(self):
         super().__init__()
         little_font = ("TT Travels text",20)
-        big_fon = ("TT Travels text",25)
+        big_font = ("TT Travels text",25)
         # Настройка окна
         self.title("Калькулятор мощности.py")
         self.geometry("400x600")
-        self.label = ctk.CTkLabel(self, text="Калькулятор установочной\nмощности двигателя", font=big_fon)
+        self.label = ctk.CTkLabel(self, text="Калькулятор установочной\nмощности двигателя", font=big_font)
         self.label.pack(pady=20)
-        self.entryH = ctk.CTkEntry(self, placeholder_text = "Введите H",font = little_font,width=200)
+        self.entryH = ctk.CTkEntry(self, placeholder_text = "Введите H (м)",font = little_font,width=250)
         self.entryH.pack(pady=20)
-        self.entryVt = ctk.CTkEntry(self, placeholder_text = "Введите Vt", font= little_font,width= 200)
+        self.entryVt = ctk.CTkEntry(self, placeholder_text = "Введите Vт (м3/c)", font= little_font,width= 250)
         self.entryVt.pack(pady=20)
-        self.entryt = ctk.CTkEntry(self, placeholder_text = "Введите t", font = little_font,width=200)
-        self.entryt.pack(pady=20)
+        self.entryTemperature = ctk.CTkEntry(self, placeholder_text = "Введите t (°С)", font = little_font,width=250)
+        self.entryTemperature.pack(pady=20)
+
+
         # Добавление виджетов
-
-
-        self.button = ctk.CTkButton(self, text="Рассчитать", command=self.button_click,font =little_font)
+        self.button = ctk.CTkButton(self, text="Рассчитать", command=self.button_click,font = big_font,width=200, height=200)
         self.button.pack(pady=100)
 
     def button_click(self):
-        print("Кнопка нажата!")
+
+        try:
+            H = float(self.entryH.get())
+            Vt = float(self.entryVt.get())
+            temperature = float(self.entryTemperature.get())
+
+            messagebox.showinfo("Готово",f"~{installed_capacity(nominal_power(useful_power(Vt,temperature)))[0]} Вт")
+        except Exception as exp:
+            messagebox.showerror("Ошибка", "Данные введены неверно!")
+            self.entryH.delete(0,ctk.END)
+            self.entryH.delete(0, ctk.END)
+            self.entryH.delete(0, ctk.END)
+            print(exp)
+
 
 if __name__ == "__main__":
     app = App()
